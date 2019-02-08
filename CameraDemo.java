e9895a0ef6519d2e379c9f2bdf23a5b976170c51
Yulia Yasnyska <yuliayasso@gmail.com>
2019-02-08 15:16:19 +0200

    Main

--- CameraDemo.java
+++ CameraDemo.java
@@ -1,14 +1,14 @@
-
 public class CameraDemo {
 
-	public static void main(String[] args) {
-		Camera b2 = new Camera();
-		Camera b1 = new Camera();
-		Camera b3 = new Camera("Peter", 84.7, 24, 11, 1000, 2, 1100, 120);
-		b3.ToString();
-		b3.resetValues("Mary", 32.5, 58, 10, 2000, 2, 2500, 150);
-		b3.ToString();
-		b1.printID();
-		Camera.printStaticID();
-	}
+ public static void main(String[] args) {
+  Camera b2 = new Camera();
+  Camera b1 = new Camera();
+  Camera b3 = new Camera("Gary", 84.7, 24, 11);
+  b3.ToString();
+  b2.resetValues("Mary", 32.5, 58, 10, 2000, 2500, 150);
+  b2.ToString();
+  b1.ToString();
+  b1.printID();
+  Camera.printStaticID();
+ }
 }
