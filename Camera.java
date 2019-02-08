2b087078c0878f1ca9d156d51e71e7825ac2de73
Yulia Yasnyska <yuliayasso@gmail.com>
2019-02-08 15:15:16 +0200

    Class Camera

--- Camera.java
+++ Camera.java
@@ -1,4 +1,3 @@
-
 class Camera {
 private String NameOfTheManufacturer;
 private double memoryCapacity;
@@ -10,76 +9,73 @@ protected int battery_capacity;
 protected int warranty_period;
 
 public Camera(){
-	
+ 
 }
 
 
 public Camera(String NameOfTheManufacturer, double memoryCapacity, int multiplicity_of_zoom, int color){
-	this.NameOfTheManufacturer = NameOfTheManufacturer;
-	this.memoryCapacity = memoryCapacity;
-	this.multiplicity_of_zoom = multiplicity_of_zoom;
-	this.color = color;
+ this.NameOfTheManufacturer = NameOfTheManufacturer;
+ this.memoryCapacity = memoryCapacity;
+ this.multiplicity_of_zoom = multiplicity_of_zoom;
+ this.color = color;
 }
 
 
 public Camera(String NameOfTheManufacturer, double memoryCapacity, int multiplicity_of_zoom, int color, int price, int ID, int battery_capacity, int warranty_period){
-	this.NameOfTheManufacturer = NameOfTheManufacturer;
-	this.memoryCapacity = memoryCapacity;
-	this.multiplicity_of_zoom = multiplicity_of_zoom;
-	this.color = color;
-	this.price = price;
-	this.battery_capacity = battery_capacity;
-	this.warranty_period = warranty_period;
+ this (NameOfTheManufacturer, memoryCapacity, multiplicity_of_zoom, color);
+ this.price = price;
+ this.battery_capacity = battery_capacity;
+ this.warranty_period = warranty_period;
 }
 
 
 public String getNameOfTheManufacturer(){
-	return NameOfTheManufacturer;
+ return NameOfTheManufacturer;
 }
 
 
 public void setNameOfTheManufacturer(String s){
-	this.NameOfTheManufacturer = s;
+ this.NameOfTheManufacturer = s;
 }
 
 
 public double getMemoryCapacity(){
-	return memoryCapacity;
+ return memoryCapacity;
 }
 
 
 public void setMemoryCapacity(double k){
-	this.memoryCapacity = k;
+ this.memoryCapacity = k;
 }
 
 
 public int getMultiplicity_of_zoom(){
-	return multiplicity_of_zoom;
+ return multiplicity_of_zoom;
 }
 
 
 public void setMultiplicity_of_zoom(int b){
-	this.multiplicity_of_zoom = b;
+ this.multiplicity_of_zoom = b;
 }
 
 
 public int getColor(){
-	return color;
+ return color;
 }
 
 
 public void setColor(int a){
-	this.color = a;
+ this.color = a;
 }
 
 
 public int getPrice(){
-	return price;
+ return price;
 }
 
 
 public void setPrice(int c){
-	this.price = c;
+ this.price = c;
 }
 
 
@@ -104,7 +100,7 @@ public void setWarranty_period(int z){
 
 
 public void ToString(){
-	System.out.println("***************************** \n Name Of The Manufacturer: "+ NameOfTheManufacturer +"\n Memory Capacity: "+ memoryCapacity +"\n Multiplicity of zoom: "+ multiplicity_of_zoom +"\n Color: "+ color +"\n Price: "+ price +"\n ID: "+ ID +"\n Battery Capacity: "+ battery_capacity +"\n Warranty Period: "+ warranty_period);
+ System.out.println("***************************** \n Name Of The Manufacturer: "+ NameOfTheManufacturer +"\n Memory Capacity: "+ memoryCapacity +"\n Multiplicity of zoom: "+ multiplicity_of_zoom +"\n Color: "+ color +"\n Price: "+ price +"\n Battery Capacity: "+ battery_capacity +"\n Warranty Period: "+ warranty_period);
 }
 
 
@@ -115,18 +111,19 @@ public static int printStaticID(){
 
 
 public int printID(){
-	System.out.println("***************************** \n ID: "+ ID);
+ System.out.println("***************************** \n ID: "+ ID);
     return ID;
 }
 
 
-public void resetValues(String NameOfTheManufacturer, double memoryCapacity, int multiplicity_of_zoom, int color, int price, int ID, int battery_capacity, int warranty_period){
-	this.NameOfTheManufacturer = NameOfTheManufacturer;
-	this.memoryCapacity = memoryCapacity;
-	this.multiplicity_of_zoom = multiplicity_of_zoom;
-	this.color = color;
-	this.price = price;
-	this.battery_capacity = battery_capacity;
-	this.warranty_period = warranty_period;
+public void resetValues(String NameOfTheManufacturer, double memoryCapacity, int multiplicity_of_zoom, int color, int price, int battery_capacity, int warranty_period){
+ this.NameOfTheManufacturer = NameOfTheManufacturer;
+ this.memoryCapacity = memoryCapacity;
+ this.multiplicity_of_zoom = multiplicity_of_zoom;
+ this.color = color;
+ this.price = price;
+ this.battery_capacity = battery_capacity;
+ this.warranty_period = warranty_period;
 }
 }
+
