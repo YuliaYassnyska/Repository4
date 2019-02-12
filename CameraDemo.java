e673c2ca386110a3e04eadba77c65faaa5ae70dd
Yulia Yasnyska <yuliayasso@gmail.com>
2019-02-12 05:44:43 -0800

    Main

--- CameraDemo.java
+++ CameraDemo.java
@@ -5,10 +5,10 @@ public class CameraDemo {
 	 public static void main(String[] args) {
 		 Camera object1 = new Camera();
 		 Camera object2 = new Camera("Gary", 84.7, 24, 11);
-		 Camera object3 = new Camera("Mary", 32.5, 58, 10, 2000, 2, 2500, 150);
-		 object1.toString();
-		 object2.toString();
-		 object3.toString();
+		 Camera object3 = new Camera("Mary", 32.5, 58, 10, 2000, 2500, 150);
+		 System.out.println(object1.toString());
+		 System.out.println(object2.toString());
+		 System.out.println(object3.toString());
 
 		 object1.printID();
 		 Camera.printStaticID();
